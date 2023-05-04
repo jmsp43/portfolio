d2576f2ef44f45203199c5c9cc51a11644b8d400
Jenna Perez <jmsp43@gmail.com>
2023-05-04 11:52:59 -0400

    letter placement fixed

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,14 @@
 import './index.scss'
 import LogoJ from '../../assets/images/logoJ.png'
+import cakeE from '../../assets/images/cakeE.png'
+import cakeN from '../../assets/images/cakeN.png'
+import cakeA from '../../assets/images/cakeA.png'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
     const [letterClassState, setLetterClassState] = useState('text-animate')
-    const nameArr = 'enna'.split()
     const careerArr = 'Software Engineer'.split()
 
     useEffect(() => {
@@ -27,10 +29,13 @@ const Home = () => {
                         <span className={`${letterClassState} _14`}> I</span>
                         <span className={`${letterClassState} _15`}>'m</span>
                         <br />
-                        <AnimatedLetters letterClass={letterClassState} strArray={nameArr} index={16} />
-                        <img src={LogoJ} alt='letter logo' />
+                        <img src={LogoJ} width = '125px' height = '125px' alt='letter logo' />
+                        <img src={cakeE} width = '90px' height = '90px' alt='letter logo' />
+                        <img src={cakeN} width = '125px' height = '125px' alt='letter logo' />
+                        <img src={cakeN} width = '125px' height = '125px' alt='letter logo' />
+                        <img src={cakeA} width = '110px' height = '110px' alt='letter logo' />
                         <br />
-                        <AnimatedLetters letterClass={letterClassState} strArray={careerArr} index={20} />
+                        <AnimatedLetters letterClass={letterClassState} strArray={careerArr} index={16} />
                     </h1>
 
                     <h2>JavaScript | MongoDB | ExpressJS | ReactJS | NodeJS | Python | SQL </h2>
