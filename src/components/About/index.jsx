2df4835eb13cc82a9fc5befea129bb4200309d10
Jenna Perez <jmsp43@gmail.com>
2023-05-06 17:24:14 -0400

    added skills images to about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHtml5, faJsSquare, faNodeJs, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
+import mongoDB from '../../assets/images/mongoDB.png';
+import sql from '../../assets/images/sql.png'
 
 export default function About() {
   const [letterClassState, setLetterClassState] = useState('text-animate')
 
   setTimeout(() => {
     setLetterClassState('text-animate-hover')
-  }, 5000)
+  }, 2000)
   
   const aboutMe = 'About Me'.split('')
   const mapAbout = aboutMe.map((char, i) => {
@@ -20,8 +24,6 @@ export default function About() {
     <div className='container aboutPage'>
       <div className = 'textArea'>
         <h1>
-          {/* <AnimatedLetters letterClass={letterClassState} strArr={('About Me'.split())}
-            index={15} /> */}
           {mapAbout}
         </h1>
         <br/>
@@ -29,9 +31,38 @@ export default function About() {
         <br/>
         <p>I'm passionate about bringing a forward-thinking mindset and a focus on innovation and ingenuity to any project I work on, and would love to join a team with as much drive to create tangible, positive change for those who encounter our work as I do.</p>
         <br/>
-        <p>Ever since my first job as a math tutor in my sophomore year of high school, and I always dreamed of a way to not only reconcile, but harness both my problem-solving, analytical nature with my creative, free-thinking industriousness, and software engineering has done all that and more.</p>
+        <p>Ever since my first job as a math tutor in my sophomore year of high school, I always dreamt of a way to not only reconcile, but harness both my problem-solving, analytical nature with my creative, free-thinking industriousness, and software engineering has done all that and more.</p>
         <br />
-        <p>A few photos of me having the time of my life:</p>
+      </div>
+
+      <div className='cube-container'>
+        <div className='spinner'>
+          <div className = 'face1'>
+            <FontAwesomeIcon icon = {faNodeJs} color = 'chartreuse'></FontAwesomeIcon>
+          </div>
+
+          <div className = 'face2'>
+            <FontAwesomeIcon icon = {faJsSquare} color = 'yellow'></FontAwesomeIcon>
+          </div>
+
+          <div className = 'face3'>
+            <FontAwesomeIcon icon = {faReact} color = 'lightblue'></FontAwesomeIcon>
+          </div>
+
+          <div className = 'face4'>
+            <img width = '100px' height = '100px' src={mongoDB}/>
+          </div>
+
+          <div className = 'face5'>
+            <FontAwesomeIcon icon = {faPython} color = 'blue'></FontAwesomeIcon>
+          </div>
+
+          <div className = 'face6'>
+            <img width = '100px' height = '100px' src={sql}/>
+          </div>
+
+        </div>
+
       </div>
     </div>
   )
